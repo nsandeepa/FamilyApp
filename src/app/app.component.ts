96e8df98646bb8b7b293b920073a63c8bcde2bac
Nilupul Sandeepa <nilupulsandeepa@gmail.com>
2018-05-30 00:51:32 +0530

    Code fixes and bug fixes for autherizaion funtionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { SplashPage } from '../pages/splash/splash';
-
 import { LoginPage } from '../pages/login/login';
-import { SignUpPage } from '../pages/sign-up/sign-up';
+
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {]
-  rootPage:any = SignUpPage;
+export class MyApp {
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+
 import { SignupUser } from '../../models/SignupUser';
 import { LoginPage } from '../login/login';
 import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service'
 import { FirebaseListener } from '../../providers/firebase-service/FirebaseListener';
-import { FirebaseAuthError } from '../../providers/firebase-service/FirebaseAuthError'; 
+import { FirebaseAuthError } from '../../providers/firebase-service/FirebaseAuthError';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the SignUpPage page.
@@ -20,80 +22,65 @@ import { FirebaseAuthError } from '../../providers/firebase-service/FirebaseAuth
 })
 export class SignUpPage {
 
-  signupUser: SignupUser ={
-      email: "",
-      password: "",
-      name: "",
-      confirmPassword: ""
+  public signupUser: SignupUser = {
+    email: "",
+    password: "",
+    name: "",
+    confirmPassword: ""
   }
 
   constructor(
-      public navCtrl: NavController,
-      public navParams: NavParams,
-      private alertCtrl: AlertController,
-      public firebaseService: FirebaseServiceProvider) {
-          this.firebaseService.setFirebaseListener(this);
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private alertCtrl: AlertController,
+    public firebaseService: FirebaseServiceProvider
+  ) {
+    this.firebaseService.setFirebaseListener(this);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignUpPage');
-  }
-
-  signUp(){
-
-    console.log(this.signupUser.name);
-    console.log(this.signupUser.email);
-    console.log(this.signupUser.password);
-    console.log(this.signupUser.confirmPassword);
-
-   if(!this.signupUser.name || !this.signupUser.email || !this.signupUser.password || !this.signupUser.confirmPassword){
-        let alert1 = this.alertCtrl.create({
-          title: 'Login Error',
-          subTitle: 'Fields cannot be empty',
-          buttons: ['Dismiss']
-        });
-
-        alert1.present();
-
-   } else if(!(this.signupUser.password === this.signupUser.confirmPassword)){
-        let alert2 = this.alertCtrl.create({
-          title: 'Login Error',
-          subTitle: 'Password not mached',
-          buttons: ['Dismiss']
-        });
-        alert2.present();
-    } else if((this.signupUser.password.length<6 )){
-        let alert2 = this.alertCtrl.create({
-          title: 'Login Error',
-          subTitle: 'Password must be at least 6 characters',
-          buttons: ['Dismiss']
-        });
-        alert2.present();
-   }else{
-        this.signupUser.email = this.signupUser.email.concat( "@gmail.com" );
-        this.firebaseService.signUpUser(this.signupUser);
-
-   }
+  signUp() {
+    if (!this.signupUser.name || !this.signupUser.email || !this.signupUser.password || !this.signupUser.confirmPassword) {
+      this.showAlert("Login Error", "Fields cannot be empty!");
+    } else if (!(this.signupUser.password === this.signupUser.confirmPassword)) {
+      this.showAlert("Login Error", "Password does not match!");
+    } else if ((this.signupUser.password.length < 6)) {
+      this.showAlert("Login Error", "Password must be at least 6 characters.");
+    } else {
+      this.signupUser.email = this.signupUser.email.concat("@gmail.com");
+      this.firebaseService.signUpUser(this.signupUser);
+    }
   }
 
   goToSignIn() {
     this.navCtrl.push(LoginPage);
   }
 
-  OnSignUpComplete(email: string){
-    console.log("success");
+  OnSignUpComplete(email: string): void {
+    this.navCtrl.setRoot(HomePage);
+  }
+
+  OnSignInComplete(email: string): void {
 
   }
-  OnSignInComplete(email: string){
+
+  OnSignInCheck(email: string): void {
+
+  }
+
+  OnSignOutComplete(): void {
+
+  }
   
-  }
-  OnSignInCheck(email: string){
+  OnAuthError(error: FirebaseAuthError): void {
 
   }
-  OnSignOutComplete(){
 
-  }
-  OnAuthError(error: FirebaseAuthError){
-
+  showAlert(title: string, message: string): void {
+    let alertMsg = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: ['Dismiss']
+    });
+    alertMsg.present();
   }
 }
